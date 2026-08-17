e05cfd05fe4739eb4e9bca85247a607b1fbbabb9
Dmitry-Dyagilev <dyagilevde@gmail.com>
2026-08-17 16:47:51 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
-import "fmt"
-func main(){
-fmt.Println("Hello, world!")
+
+func main() {
+
+}
+
+func MaxInt(a, b int) int {
+    if a >= b {
+        return a
+    }
+
+    return b
 }
